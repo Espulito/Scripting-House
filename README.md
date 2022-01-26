a4b68fc384bb1b9ebdd5faf0508a1490e0e4cc55
Martin Reale / Espulito <espulito@gmail.com>
2022-01-26 18:50:42 -0300

    Primer commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# **The Scripting House**
+
+### Esta es la página en *construcción* de la comunidad de Discord **"La Casa del Scripting"** o **"The Scripting House"**.
---
+++ main.js
@@ -0,0 +1,16 @@
+// Typed.js
+
+const typed = new Typed(".typed", {
+  strings: ["Under construction."],
+
+  typeSpeed: 60,
+  startDelay: 500,
+  backSpeed: 40,
+  shuffle: false,
+  backDelay: 5000,
+  loop: true,
+  loopCount: false,
+  showCursor: true,
+  cursorChar: "|",
+  contentType: "html",
+});
